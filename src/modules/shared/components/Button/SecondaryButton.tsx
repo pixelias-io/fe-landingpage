978229770e660dcffeab3dcdba235f862adf416c
Matheus Moreira <mattelias@live.com>
2023-08-15 00:44:36 -0300

    feat(button): add secondary button

---
+++ src/modules/shared/components/Button/SecondaryButton.tsx
@@ -0,0 +1,16 @@
+// create a component in typescript for a primary button
+
+import React, { FC, ReactNode, ButtonHTMLAttributes } from "react";
+
+interface SecondaryButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
+  children: ReactNode;
+  className?: string;
+}
+
+export const SecondaryButton: FC<SecondaryButtonProps> = ({ children, className = '' }) => {
+  return (
+    <button className={`border border-crimson-pink hover:bg-crimson-pink-100 font-bold text-xl text-crimson-pink rounded-lg py-4 px-8 ${className}`}>
+      {children}
+    </button>
+  );
+};
