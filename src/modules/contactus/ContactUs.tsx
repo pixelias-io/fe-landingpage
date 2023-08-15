d060119e41535b125819a38be66d20dd546eb673
Matheus Moreira <mattelias@live.com>
2023-08-15 18:32:23 -0300

    refactor(contactus): extract FORM_SUBMIT_TOKEN as a env

--- src/modules/contactus/ContactUs.tsx
+++ src/modules/contactus/ContactUs.tsx
@@ -3,7 +3,11 @@ import { Heading } from "../shared/components/Heading/Heading";
 import { Input } from "../shared/components/Input/Input";
 import { TextArea } from "../shared/components/Input/TextArea";
 
+const FORM_SUBMIT_TOKEN = process.env.NEXT_PUBLIC_FORM_SUBMIT_TOKEN;
+
 export const ContactUs = () => {
+  const action = `https://formsubmit.co/${FORM_SUBMIT_TOKEN}`;
+
   return (
     <section className="w-full lg:p-24 p-8 pt-16">
       <Heading as="h2">Want to start a project?</Heading>
@@ -11,22 +15,18 @@ export const ContactUs = () => {
       <div className="grid lg:grid-cols-2 gap-16">
         <div className="lg:text-xl text-lg">
           <p>
-            Whether you&apos;re a visionary entrepreneur, an established business, or
-            anyone in between, we&apos;re excited to collaborate and bring your
-            digital aspirations to life.
+            Whether you&apos;re a visionary entrepreneur, an established
+            business, or anyone in between, we&apos;re excited to collaborate
+            and bring your digital aspirations to life.
           </p>
 
           <p className="mt-4">
-            Let&apos;s connect, discuss, and create something amazing together! Your
-            journey to digital excellence starts here.
+            Let&apos;s connect, discuss, and create something amazing together!
+            Your journey to digital excellence starts here.
           </p>
         </div>
 
-        <form
-          className="flex flex-col gap-6"
-          action="https://formsubmit.co/983efe2e3085464cc46df044ce306028"
-          method="POST"
-        >
+        <form className="flex flex-col gap-6" action={action} method="POST">
           <Input type="text" name="name" placeholder="Your name" required>
             Name
           </Input>
@@ -53,7 +53,9 @@ export const ContactUs = () => {
             Message
           </TextArea>
 
-          <PrimaryButton className="self-start" type="submit">Send Message</PrimaryButton>
+          <PrimaryButton className="self-start" type="submit">
+            Send Message
+          </PrimaryButton>
         </form>
       </div>
     </section>
