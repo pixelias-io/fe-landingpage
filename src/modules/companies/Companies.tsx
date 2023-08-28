1c6b55b5f618665f14a158904d384fd35ca79808
Matheus Moreira <mattelias@live.com>
2023-08-28 16:20:35 -0300

    refactor(Companies): extract list of companies

--- src/modules/companies/Companies.tsx
+++ src/modules/companies/Companies.tsx
@@ -1,24 +1,7 @@
 import Image from "next/image";
 import { Card } from "../shared/components/Card/Card";
 import { FC } from "react";
-
-const companies = [
-  {
-    name: "IVECO",
-    logoSrc: "/iveco-logo.svg",
-    url: "https://www.iveco.com/",
-  },
-  {
-    name: "DAZN",
-    logoSrc: "/dazn-logo.svg",
-    url: "https://www.dazn.com/",
-  },
-  {
-    name: "Hotmart",
-    logoSrc: "/hotmart-logo.svg",
-    url: "https://www.hotmart.com/",
-  },
-];
+import { companies } from "./constants";
 
 interface CompanyProps {
   url: string;
---
+++ src/modules/companies/constants.ts
@@ -0,0 +1,17 @@
+export const companies = [
+  {
+    name: "IVECO",
+    logoSrc: "/iveco-logo.svg",
+    url: "https://www.iveco.com/",
+  },
+  {
+    name: "DAZN",
+    logoSrc: "/dazn-logo.svg",
+    url: "https://www.dazn.com/",
+  },
+  {
+    name: "Hotmart",
+    logoSrc: "/hotmart-logo.svg",
+    url: "https://www.hotmart.com/",
+  },
+];
