433095493ece92f18330d9696d29540ea9eeb468
Matheus Moreira <mattelias@live.com>
2023-08-28 16:14:37 -0300

    test: change jsx transformer

--- jest.config.js
+++ jest.config.js
@@ -3,4 +3,15 @@ module.exports = {
   preset: "ts-jest",
   testEnvironment: "jsdom",
   setupFilesAfterEnv: ["<rootDir>/jest.setup.ts"],
-};  
+  testPathIgnorePatterns: ["<rootDir>/node_modules", "<rootDir>/dist"],
+  moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
+  modulePaths: ["<rootDir>"],
+  transform: {
+    "^.+\\.tsx?$": [
+      "ts-jest",
+      {
+        tsconfig: "<rootDir>/tsconfig.test.json",
+      },
+    ],
+  },
+};
